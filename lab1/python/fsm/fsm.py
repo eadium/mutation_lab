5a48b1401f7dfe976bf9132d0baf5e26df763721
Artem Andryukhov <a.andryuhov@city-mobil.ru>
2020-11-22 00:13:45 +0300

    upd fsm

--- lab1/python/fsm/fsm.py
+++ lab1/python/fsm/fsm.py
@@ -1,16 +1,9 @@
-
 class FSM:
     d1 = False
     d2 = False
 
-    def reset(self):
-        self.d1 = False
-        self.d2 = False
-
-    def process(self, i):
+    def process(self, i: bool) -> bool:
         output = i and not self.d2
         self.d2 = output and self.d1
         self.d1 = output
-        # print("state: {}{}".format(int(self.d1), int(self.d2)))
         return bool(output)
-
