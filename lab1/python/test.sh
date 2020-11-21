21869ed865a3d7f66d612f6936f4f3d4266507d8
Artem Andryukhov <a.andryuhov@city-mobil.ru>
2020-11-22 01:37:15 +0300

    added test.sh

---
+++ lab1/python/test.sh
@@ -0,0 +1,10 @@
+s=0;
+(python3 -m unittest > /dev/null 2>&1 && echo Run tests without mutations OK) || (echo "Run tests without mutations FAILED" && exit 1);
+cp ./fsm/fsm.py ./mutants/fsm.py
+for i in $(seq 20 $END); 
+do 
+    cp "./mutants/$i.py" ./fsm/fsm.py;
+    python3 -m unittest > /dev/null 2>&1 && echo Mutant $i survived! && ((s=s+1));
+done
+cp ./mutants/fsm.py ./fsm/fsm.py
+echo Made $i tests, $s survived
