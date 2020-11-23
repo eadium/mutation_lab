79d86eb445ecc8323875b6d69a1c03cf6bff3daf
Artem Andryukhov <a.andryuhov@city-mobil.ru>
2020-11-23 20:25:28 +0300

    upd increment

--- lab1/python/test.sh
+++ lab1/python/test.sh
@@ -4,7 +4,7 @@ cp ./fsm/fsm.py ./mutants/fsm.py
 for i in $(seq 20 $END); 
 do 
     cp "./mutants/$i.py" ./fsm/fsm.py;
-    python3 -m unittest > /dev/null 2>&1 && echo Mutant $i survived! && ((s=s+1));
+    python3 -m unittest > /dev/null 2>&1 && echo Mutant $i survived! && ((s+=1));
 done
 cp ./mutants/fsm.py ./fsm/fsm.py
 echo Made $i tests, $s survived
